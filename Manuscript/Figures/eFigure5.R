e1e33237ea84bb12e342e7ce3bc8f01ae2557a4a
FionaAHagenbeek <119841768+FionaAHagenbeek@users.noreply.github.com>
2026-02-18 17:14:58 +0100

    Update eFigure5.R
    add random effects meta analytical results to plot code

--- Manuscript/Figures/eFigure5.R
+++ Manuscript/Figures/eFigure5.R
@@ -16,7 +16,7 @@ rm(list=ls())
 #
 # Data: FGR11 + UKB + GS + meta-analysis model 2
 #
-# Last edits: 01/07/2025 (edits, FAH: update & check script prior to upload to GitHub)
+# Last edits: 18/02/2026 (edits, FAH: add random-effect meta-analytical results)
 # 
 ################################################################################
 
@@ -61,14 +61,17 @@ packages("data.table","ggplot2","viridis","dplyr","forcats","stringr","cowplot",
 FGR11.2 <- fread("output/GoogleDrive/FGR11/2024-03-13_FinnGenR11_INTERVENE_EducationalAttainment_CoxPH_model2_Coeffs.txt", data.table=FALSE)
 FGR11.2$Biobank <- "FinnGen"
 #
-UKB.2 <- fread("output/GoogleDrive/UKB/2025-05-22_UKBiobank_EUR_INTERVENE_EducationalAttainment_CoxPH_model2_Coeffs.txt",data.table=FALSE)
-UKB.2$Biobank <- "UK Biobank"
+UKB.2.EUR <- fread("output/GoogleDrive/UKB/2025-05-22_UKBiobank_EUR_INTERVENE_EducationalAttainment_CoxPH_model2_Coeffs.txt",data.table=FALSE)
+UKB.2.EUR$Biobank <- "UK Biobank"
 #
 GS.2 <- fread("output/GoogleDrive/GS/2024-07-04_GS_INTERVENE_EducationalAttainment_CoxPH_model2_Coeffs.txt",data.table=FALSE)
 GS.2$Biobank <- "Generation Scotland"
 #
 FEMA.2 <- as.data.frame(read_excel("output/GoogleDrive/MetaAnalysis/2025-05-22_INTERVENE_EducationalAttainment_FEMetaAnalysis_FinnGenR11_UKB_GenScot_model2.xlsx"))
 FEMA.2$Biobank <- "FE meta-analysis"
+#
+REMA.2 <- fread("output/GoogleDrive/MetaAnalysis/2026-02-18_INTERVENE_EducationalAttainment_REMetaAnalysis_FinnGenR11_UKB_GenScot_model2.csv", data.table = FALSE)
+REMA.2$Biobank <- "RE meta-analysis"
 
 
 ################################################################################
@@ -80,7 +83,11 @@ FEMA.2$Biobank <- "FE meta-analysis"
 ################################################################################
 
 ## remove traits not meta-analyzed ##
+# fixed effect
 FEMA.2 <- FEMA.2[-which(FEMA.2$Phenotype %in% c("I9_AF","C3_COLORECTAL")),]
+
+# random effect
+REMA.2 <- REMA.2[-which(REMA.2$Phenotype %in% c("I9_AF","C3_COLORECTAL")),]
 
 
 ################################################################################
@@ -90,49 +97,49 @@ FEMA.2 <- FEMA.2[-which(FEMA.2$Phenotype %in% c("I9_AF","C3_COLORECTAL")),]
 ################################################################################
 
 # create dataset combining results for model 2
-TS6 <- data.frame(Biobank = c(FEMA.2$Biobank,
+TS6 <- data.frame(Biobank = c(FEMA.2$Biobank,REMA.2$Biobank,
                               FGR11.2$Biobank,FGR11.2$Biobank,
-                              UKB.2$Biobank,UKB.2$Biobank,
+                              UKB.2.EUR$Biobank,UKB.2.EUR$Biobank,
                               GS.2$Biobank,GS.2$Biobank),
-                  Test = c(FEMA.2$Test,
+                  Test = c(FEMA.2$Test,REMA.2$Test,
                            rep("high educational attainment",nrow(FGR11.2)), rep("PGS",nrow(FGR11.2)), 
-                           rep("high educational attainment", nrow(UKB.2)), rep("PGS",nrow(UKB.2)), 
+                           rep("high educational attainment", nrow(UKB.2.EUR)), rep("PGS",nrow(UKB.2.EUR)), 
                            rep("high educational attainment",nrow(GS.2)), rep("PGS",nrow(GS.2))),
-                  Phenotype = c(FEMA.2$Phenotype,
+                  Phenotype = c(FEMA.2$Phenotype,REMA.2$Phenotype,
                                 FGR11.2$trait,FGR11.2$trait,
-                                UKB.2$trait,UKB.2$trait,
+                                UKB.2.EUR$trait,UKB.2.EUR$trait,
                                 GS.2$trait,GS.2$trait),
-                  Beta = c(FEMA.2$Beta,
+                  Beta = c(FEMA.2$Beta,REMA.2$Beta,
                            FGR11.2$EAhigh_beta,FGR11.2$PRS_beta,
-                           UKB.2$EAhigh_beta,UKB.2$PRS_beta,
+                           UKB.2.EUR$EAhigh_beta,UKB.2.EUR$PRS_beta,
                            GS.2$EAhigh_beta,GS.2$PRS_beta),
-                  SE = c(FEMA.2$SE,
+                  SE = c(FEMA.2$SE,REMA.2$SE,
                          FGR11.2$EAhigh_se,FGR11.2$PRS_se,
-                         UKB.2$EAhigh_se,UKB.2$PRS_se,
+                         UKB.2.EUR$EAhigh_se,UKB.2.EUR$PRS_se,
                          GS.2$EAhigh_se,GS.2$PRS_se),
-                  Pval = c(FEMA.2$Pval,
+                  Pval = c(FEMA.2$Pval,REMA.2$Pval,
                            FGR11.2$EAhigh_p,FGR11.2$PRS_p,
-                           UKB.2$EAhigh_p,UKB.2$PRS_p,
+                           UKB.2.EUR$EAhigh_p,UKB.2.EUR$PRS_p,
                            GS.2$EAhigh_p,GS.2$PRS_p),
-                  HR = c(FEMA.2$HR,
+                  HR = c(FEMA.2$HR,REMA.2$HR,
                          FGR11.2$EAhigh_HR,FGR11.2$PRS_HR,
-                         UKB.2$EAhigh_HR,UKB.2$PRS_HR,
+                         UKB.2.EUR$EAhigh_HR,UKB.2.EUR$PRS_HR,
                          GS.2$EAhigh_HR,GS.2$PRS_HR),
-                  Cineg = c(FEMA.2$Cineg,
+                  Cineg = c(FEMA.2$Cineg,REMA.2$Cineg,
                             FGR11.2$EAhigh_HR_lower95,FGR11.2$PRS_HR_lower95,
-                            UKB.2$EAhigh_HR_lower95,UKB.2$PRS_HR_lower95,
+                            UKB.2.EUR$EAhigh_HR_lower95,UKB.2.EUR$PRS_HR_lower95,
                             GS.2$EAhigh_HR_lower95,GS.2$PRS_HR_lower95),
-                  Cipos = c(FEMA.2$Cipos,
+                  Cipos = c(FEMA.2$Cipos,REMA.2$Cipos,
                             FGR11.2$EAhigh_HR_upper95,FGR11.2$PRS_HR_upper95,
-                            UKB.2$EAhigh_HR_upper95,UKB.2$PRS_HR_upper95,
+                            UKB.2.EUR$EAhigh_HR_upper95,UKB.2.EUR$PRS_HR_upper95,
                             GS.2$EAhigh_HR_upper95,GS.2$PRS_HR_upper95),
-                  QHet = c(FEMA.2$QHet,
+                  QHet = c(FEMA.2$QHet,REMA.2$QHet,
                            rep(NA,2*nrow(FGR11.2)),
-                           rep(NA,2*nrow(UKB.2)),
+                           rep(NA,2*nrow(UKB.2.EUR)),
                            rep(NA,2*nrow(GS.2))),
-                  HetPval = c(FEMA.2$HetPval,
+                  HetPval = c(FEMA.2$HetPval,REMA.2$HetPval,
                               rep(NA,2*nrow(FGR11.2)),
-                              rep(NA,2*nrow(UKB.2)),
+                              rep(NA,2*nrow(UKB.2.EUR)),
                               rep(NA,2*nrow(GS.2))))
 # adjust labels
 TS6$Test[which(TS6$Test=="EA")] <- "high educational attainment"
@@ -153,8 +160,10 @@ TS6$Phenotype <- factor(TS6$Phenotype, levels = c("T1D","C3_PROSTATE","T2D","GOU
                                    "Major Depression","Any Cancer","Epilepsy",
                                    "Appendicitis","Alcohol Use Disorder"))
 # Biobank as factor
-TS6$Biobank <- factor(TS6$Biobank, levels = c("FE meta-analysis","FinnGen","UK Biobank", "Generation Scotland"), 
-                      labels = c("FE meta-analysis","FinnGen","UK Biobank","Generation Scotland"))
+TS6$Biobank <- factor(TS6$Biobank, levels = c("FE meta-analysis","RE meta-analysis",
+                                              "FinnGen","UK Biobank", "Generation Scotland"), 
+                      labels = c("FE meta-analysis","RE meta-analysis",
+                                              "FinnGen","UK Biobank", "Generation Scotland"))
 
 
 ################################################################################
@@ -174,7 +183,7 @@ FigS5 <- ggplot(TS6, aes(x = HR, y = Biobank,
   scale_shape_manual(values=c(15,16))+
   geom_linerange(position = position_dodge(1),size=5) +
   theme_minimal() + scale_colour_manual(values= c("#BC65DB","black")) + geom_vline(xintercept = 1, lty = 2,size = 2) +
-  geom_hline(yintercept=seq(0.5,18.5,1),color="black",size=.5) +
+  geom_hline(yintercept=seq(0.5,5.5,1),color="black",size=.5) +
   scale_x_continuous(transform = scales::transform_log(), 
                      breaks = c(0.2,0.3,0.4,0.5,0.7,1.1, 1.5, 2, 2.5, 3)) +
   theme(axis.title.y = element_blank(),legend.title = element_blank(), 
@@ -190,3 +199,4 @@ png(filename = paste0("output/Figures/Manuscript/",as.character(Sys.Date()),
      width=700,height=600,units='mm',res=600,pointsize=6)
 FigS5
 dev.off()
+
