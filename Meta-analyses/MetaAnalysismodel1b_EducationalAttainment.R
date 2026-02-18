83772eb8a25a7282cdd95dc6a9d436982b8f9d2d
FionaAHagenbeek <119841768+FionaAHagenbeek@users.noreply.github.com>
2026-02-18 10:50:36 +0100

    Update MetaAnalysismodel1b_EducationalAttainment.R
    fix typo

--- Meta-analyses/MetaAnalysismodel1b_EducationalAttainment.R
+++ Meta-analyses/MetaAnalysismodel1b_EducationalAttainment.R
@@ -157,7 +157,7 @@ for(i in unique(all.1b$trait)){
   disease <- subset(all.1b, trait==i & !(is.na(PRS_beta)))
   
   #Meta analysis should be done at the beta level  
-  metar <- rma(yi=PRS_beta, sei=PRS_se, data=disease, method="FE")
+  metar <- rma(yi=PRS_beta, sei=PRS_se, data=disease, method="REML")
   
   metaresultsr.1b <- rbind(metaresultsr.1b, c(i, metar$b, metar$se, metar$pval, metar$QE, metar$QEp))
 }
@@ -192,5 +192,6 @@ drive_upload(media = "output/2classEA/MetaAnalysis/FGR11_UKB_GS/model1/2026-02-1
              path = as_id("1Wi0KDwGtnZoUclUgZwu7F_Dwj-6uvJYH"),
              name = "2026-02-18_INTERVENE_EducationalAttainment_REMetaAnalysis_FinnGenR11_UKB_GenScot_model1b.csv",
              type = "spreadsheet")
+
 
 
