e473b29607aced38cec79f9d17687c56824e8ff0
FionaAHagenbeek <119841768+FionaAHagenbeek@users.noreply.github.com>
2026-03-12 10:33:19 +0100

    Update MetaAnalysismodel1b_EducationalAttainment.R
    to comment to combine cohort-level data add GenScot

--- Meta-analyses/MetaAnalysismodel1b_EducationalAttainment.R
+++ Meta-analyses/MetaAnalysismodel1b_EducationalAttainment.R
@@ -104,7 +104,7 @@ GS.1b$Biobank <- "Generation Scotland"
 
 ################################################################################
 #
-# Combine FGR11 & UKB results
+# Combine FGR11, UKB & GenScot results
 #
 ################################################################################
 
@@ -192,6 +192,7 @@ drive_upload(media = "output/2classEA/MetaAnalysis/FGR11_UKB_GS/model1/2026-02-1
              path = as_id("1Wi0KDwGtnZoUclUgZwu7F_Dwj-6uvJYH"),
              name = "2026-02-18_INTERVENE_EducationalAttainment_REMetaAnalysis_FinnGenR11_UKB_GenScot_model1b.csv",
              type = "spreadsheet")
+
 
 
 
